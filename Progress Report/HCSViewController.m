14266afe7f0792d4a45b37fad458ef6968fa9b27
Roger Z <the.roger.zou@gmail.com>
2014-06-26 01:06:09 -0700

    iCal method scheduleEvent works.

--- Progress Report/HCSViewController.m
+++ Progress Report/HCSViewController.m
@@ -7,17 +7,72 @@
 //
 
 #import "HCSViewController.h"
+#import <EventKit/EventKit.h>
+//@import EventKitUI;
 
 @interface HCSViewController ()
+
+@property (weak, nonatomic) IBOutlet UIButton *bigButton;
+@property (weak, nonatomic) IBOutlet UITextField *titleButton;
+@property (nonatomic) BOOL isStart;
+@property (strong, nonatomic) NSDate *startDate;
+@property (strong, nonatomic) NSDate *endDate;
 
 @end
 
 @implementation HCSViewController
 
+// default isStart = YES
+- (BOOL) isStart
+{
+    if (!_isStart) _isStart = YES;
+    return _isStart;
+}
+
+- (void)scheduleEvent
+{
+    NSCalendar *cal = [[NSCalendar alloc]initWithCalendarIdentifier:NSGregorianCalendar];
+    NSLog(@"meh");
+    EKEventStore *eventStore = [[EKEventStore alloc]init];
+    EKEvent *event = [EKEvent eventWithEventStore:eventStore];
+    event.title = self.titleButton.text;
+    event.startDate = self.startDate;
+    event.endDate = self.endDate;
+    NSLog(@"much success");
+    [eventStore requestAccessToEntityType:EKEntityTypeEvent completion:^(BOOL granted, NSError *error) {
+        NSLog(@"gran");
+        if (granted) {
+            //user lets calendar access
+            [event setCalendar:[eventStore defaultCalendarForNewEvents]];
+            NSError *err;
+            [eventStore saveEvent:event span:EKSpanThisEvent error:&err];
+            NSLog(@"win");
+        } else {
+            //user no calendar access
+            [event setCalendar:[eventStore defaultCalendarForNewEvents]];
+            NSError *err;
+            [eventStore saveEvent:event span:EKSpanThisEvent error:&err];
+        }
+    }];
+}
+
+- (void)updateUI
+{
+    if (self.isStart)
+        self.bigButton.titleLabel.text = @"Start";
+    else
+        self.bigButton.titleLabel.text = @"Stop";
+    
+}
+
 - (void)viewDidLoad
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    [self updateUI];
+    //self.startDate = [NSDate date];
+    //self.endDate = [[NSDate alloc] initWithTimeInterval:600 sinceDate:self.startDate];
+    //[self scheduleEvent];
 }
 
 - (void)didReceiveMemoryWarning
