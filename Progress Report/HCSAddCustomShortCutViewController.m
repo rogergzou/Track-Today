6d6f1be42d257c6aa0e56d7fe4cffbeae217f86e
Roger Z <the.roger.zou@gmail.com>
2014-08-22 11:44:29 -0700

    Moved edit button in categoryView to be easier to press

--- Progress Report/HCSAddCustomShortCutViewController.m
+++ Progress Report/HCSAddCustomShortCutViewController.m
@@ -107,7 +107,6 @@
     // Dispose of any resources that can be recreated.
 }
 
-
 /*
 - (IBAction)imageUploadButtonTouch:(id)sender {
     [self imagePickerWithSourceType:UIImagePickerControllerSourceTypePhotoLibrary];
